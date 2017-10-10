8bc52ba601e5e6edbb73fe635fffd23ff652df94
Luca Pompei <luca.pompei@sysdata.it>
2017-10-10 15:00:28 +0200

    enabled global method security

--- src/main/java/lp/web/webtemplate/config/security/SecurityConfig.java
+++ src/main/java/lp/web/webtemplate/config/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package lp.web.webtemplate.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,7 +18,9 @@ import lp.web.webtemplate.controller.Endpoints;
  * @author lucapompei
  *
  */
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	/**
