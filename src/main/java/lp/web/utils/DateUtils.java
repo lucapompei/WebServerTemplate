640a8e00e2263e1bea8577cf2d848f16d6ae5955
Luca Pompei <lucapompei91@gmail.com>
2018-06-24 23:33:12 +0200

    Add utility class

---
+++ src/main/java/lp/web/utils/DateUtils.java
@@ -0,0 +1,53 @@
+package lp.web.utils;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * This class exposes utils to handle date operations
+ *
+ * @author lucapompei
+ */
+public class DateUtils {
+
+	/**
+	 * The date formatter
+	 */
+	private static final String DATE_FORMATTER_PATTERN = "dd-MM-yyyy";
+
+	/**
+	 * Private constructor for an utility class, construct a new {@code DateUtils}
+	 */
+	private DateUtils() {
+		// Empty implementation
+	}
+
+	/**
+	 * Calculates and returns the current week
+	 * 
+	 * @return the current week
+	 */
+	public static String getCurrentWeek() {
+		LocalDate date = LocalDate.now();
+		TemporalField woy = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+		return "W" + date.get(woy);
+	}
+
+	/**
+	 * Returns a formatted and more readable date
+	 * 
+	 * @param date,
+	 *            the date to format
+	 * 
+	 * @return a formatted date
+	 */
+	public static String getFormattedDate(Date date) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMATTER_PATTERN);
+		return simpleDateFormat.format(date);
+	}
+
+}
