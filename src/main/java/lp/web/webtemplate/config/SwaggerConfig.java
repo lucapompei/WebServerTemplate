b3c2e217e8c45f917508f2c20d990ae9582c29d7
Luca Pompei <lucapompei91@gmail.com>
2018-09-09 00:06:18 +0200

    Update swagger configuration

--- src/main/java/lp/web/webtemplate/config/SwaggerConfig.java
+++ src/main/java/lp/web/webtemplate/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package lp.web.webtemplate.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicates;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -22,6 +24,6 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+				.paths(PathSelectors.any()).paths(Predicates.not(PathSelectors.regex("/error.*"))).build();
 	}
 }
