aa6d4438f5d038f51ff3fd5fe43761b20ff392e0
Luca Pompei <lucapompei91@gmail.com>
2019-02-03 01:20:00 +0100

    update constans

--- src/main/java/lp/web/webtemplate/constants/AuthConstants.java
+++ src/main/java/lp/web/webtemplate/constants/AuthConstants.java
@@ -1,24 +1,40 @@
 package lp.web.webtemplate.constants;
 
 /**
- * This class exposes utils to handle generic auth operations
+ * This utility class exposes authorization and authentication constants
  *
  * @author lucapompei
  */
 public class AuthConstants {
 
-	/**
-	 * Private constructor for an utility class, construct a new {@code AuthUtils}
-	 */
-	private AuthConstants() {
-		throw new IllegalAccessError();
-	}
+
 
 	/**
-	 * Auth public constants
+	 * Private constructor for an utility class
+	 */
+	private AuthConstants() {
+		throw new IllegalAccessError(CommonConstants.STANDARD_MESSAGE_UTILITY_CLASS);
+	}
+
+
+
+	/**
+	 * The authorization header
 	 */
 	public static final String AUTH_HEADER = "Authorization";
+
+
+
+	/**
+	 * The prefix for basic authorization value
+	 */
 	public static final String AUTH_BASICPREFIX = "Basic ";
+
+
+
+	/**
+	 * The prefix for bearer authorization value
+	 */
 	public static final String AUTH_BEARERPREFIX = "Bearer ";
 
 }
--- src/main/java/lp/web/webtemplate/constants/EndpointConstants.java
+++ src/main/java/lp/web/webtemplate/constants/EndpointConstants.java
@@ -1,44 +1,41 @@
 package lp.web.webtemplate.constants;
 
-import lp.web.webtemplate.rest.api.DataAPI;
-import lp.web.webtemplate.rest.api.HomeController;
-import lp.web.webtemplate.rest.api.InfoAPI;
-
 /**
- * This class contains the list of endpoints used by the application
+ * This utility class expose endpoint constants
  * 
  * @author lucapompei
  *
  */
 public class EndpointConstants {
 
+
+
 	/**
-	 * Endpoints handled by {@link HomeController}
+	 * The root endpoint
 	 */
-	public static final String HOME = "/";
+	public static final String ROOT = "/";
+
+
+
+	/**
+	 * The login endpoint
+	 */
 	public static final String LOGIN = "/login";
 
-	/**
-	 * Basepath for APIs endpoint
-	 */
-	public static final String API_BASE = "/api";
+
 
 	/**
-	 * Endpoints handled by {@link DataAPI}
+	 * The about endpoint
 	 */
-	public static final String DATA = "/data";
-
-	/**
-	 * Endpoints handled by {@link InfoAPI}
-	 */
-	public static final String IS_ALIVE = "/isAlive";
 	public static final String ABOUT = "/about";
 
+
+
 	/**
-	 * Private constructor for {@link EndpointConstants}
+	 * Private constructor for an utility class
 	 */
 	private EndpointConstants() {
-		throw new IllegalAccessError();
+		throw new IllegalAccessError(CommonConstants.STANDARD_MESSAGE_UTILITY_CLASS);
 	}
 
 }
