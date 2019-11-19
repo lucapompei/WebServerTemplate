e450b3285dd6decb3e8e8ca14b1b6e5061851ea6
Luca Pompei <lucapompei91@gmail.com>
2019-11-19 23:38:43 +0100

    update CORS config

--- src/main/resources/archetype-resources/src/main/java/config/CORSFilter.java
+++ src/main/resources/archetype-resources/src/main/java/config/CORSFilter.java
@@ -42,7 +42,7 @@ public class CORSFilter implements Filter {
 
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 
 		String headers = "Content-Type, Accept, X-Requested-With, Authorization";
