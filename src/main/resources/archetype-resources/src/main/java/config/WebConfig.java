50099c419403dea89af220ac8e1636938614c3b1
Luca Pompei <lucapompei91@gmail.com>
2019-12-20 21:32:05 +0100

    Configured logback to uniquely record each request

--- src/main/resources/archetype-resources/src/main/java/config/WebConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/WebConfig.java
@@ -1,23 +1,26 @@
 #set($symbol_pound='#')#set($symbol_dollar='$')#set($symbol_escape='\')
 package ${package}.config;
 
-import javax.servlet.Filter;
-
 import java.io.IOException;
+import java.util.Date;
+import java.util.UUID;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-
 
 /**
  * This config class globally configure the web requests
@@ -26,16 +29,13 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
  *
  */
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebConfig implements Filter {
-
-
 
 	/**
 	 * The logger
 	 */
 	private static final Logger LOGGER = LoggerFactory.getLogger(WebConfig.class);
-
-
 
 	/**
 	 * Defines a common multipart resolver
@@ -47,13 +47,34 @@ public class WebConfig implements Filter {
 		return new CommonsMultipartResolver();
 	}
 
-
+	/**
+	 * Handle CORS configuring response
+	 * 
+	 * @param req
+	 * @param res
+	 */
+	private void handleCORS(ServletRequest req, ServletResponse res) {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		String headers = "Content-Type, Accept, X-Requested-With, Authorization";
+		response.setHeader("Access-Control-Allow-Headers", headers);
+		response.setHeader("Access-Control-Expose-Headers", headers);
+	}
 
 	/**
 	 * Intercepts all requests and log the elapsed time took by its execution
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
+		// Handle CORS
+		handleCORS(request, response);
+		// Configure the logger to uniquely record the request
+		MDC.clear();
+		MDC.put("uuid", UUID.randomUUID().toString() + new Date().getTime());
 		// Find servlet name
 		String name = "servlet";
 		if (request instanceof HttpServletRequest) {
