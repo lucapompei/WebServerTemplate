b41b564b52bc78965409d5b2f95e59be7d476399
luca.pompei <luca.pompei@sysdata.it>
2020-05-29 17:50:37 +0200

    enable caching

--- src/main/resources/archetype-resources/src/main/java/MainApplication.java
+++ src/main/resources/archetype-resources/src/main/java/MainApplication.java
@@ -5,6 +5,7 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.PropertySource;
  */
 @PropertySource({ "classpath:application.yml" })
 @SpringBootApplication
+@EnableCaching
 public class MainApplication {
 
 
