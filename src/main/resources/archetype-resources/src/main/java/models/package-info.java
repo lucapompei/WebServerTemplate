7088ce600ab8d2d0bcd7824cac5e57eb2539cc90
Luca Pompei <luca.pompei@sysdata.it>
2022-03-10 15:29:13 +0100

    Added models package

---
+++ src/main/resources/archetype-resources/src/main/java/models/package-info.java
@@ -0,0 +1,8 @@
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\')
+package ${package}.entities;
+
+/*
+ * This package contains all the models used for business logic purpose. This package is used to implement the service layer.
+ */
