40434b936e09923a12b5ccf15b83f33bbc8587dd
Luca Pompei <luca.pompei@globant.com>
2025-04-24 16:36:26 +0200

    Minor update

--- src/main/resources/archetype-resources/src/main/java/controllers/BaseController.java
+++ src/main/resources/archetype-resources/src/main/java/controllers/BaseController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import ${package}.constants.EndpointConstants;
 import ${package}.services.InfoService;
 
+import java.util.Objects;
+
+
 /**
  * This rest controller exposes endpoints to handle the base requests
  */
